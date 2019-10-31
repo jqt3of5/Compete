d3ccd9567cafeba259073f956b4a8ad51c947463
John Todd <jqtodd@verisk.com>
2019-10-31 17:31:38 -0600

    added a new challenge, and started another.

--- challenges.js
+++ challenges.js
@@ -1,4 +1,3 @@
-
 module.exports = [
     {
         //static
@@ -19,6 +18,16 @@ module.exports = [
         pug:'question2.pug',
         verify: challenge_two_verify, 
         generate:challenge_two_generator,
+    },
+    {
+        //static
+        id:2,
+        title:"Mod Mod", 
+        shortDescription: "Given A and B, count the number of values for x where A % x = B", 
+        points: 5, 
+        pug:'question3.pug',
+        verify: challenge_three_verify, 
+        generate:challenge_three_generator,
     }
 ]
 
@@ -83,4 +92,125 @@ function collatz(x)
     } else {
 	return 1 + collatz(x * 3 + 1)
     }
+}
+
+//Untested ====================================================
+//============================================================
+
+function challenge_three_generator()
+{
+    var A = Math.floor(Math.random() * 1000) + 1000
+    var B = Math.floor(Math.random() * A/100) + 2
+
+    return {A: A, B: B}
+}
+
+function challenge_three_verify(value, answer)
+{
+    var answerCount = parseInt(answer)
+
+    var count = 0
+    
+    for (int i = 1; i < value.A; ++i)
+    {
+	if (value.A % i == value.B)
+	{
+	    count += 1
+	}
+    }
+
+    return count == answerCount
+}
+
+//Find the shortest hamiltonian path
+function challenge_four_generator()
+{
+    var nodes = 10
+    var matrix = []
+
+    //May construct a graph with no hamiltonian path
+    for (i = 0; i < nodes; ++i)
+    {
+	var edges = []
+	for (j = 0; j < nodes; ++i)
+	{
+	    if (j == i)
+	    {
+		edges.add(0)
+	    }
+	    else if (j < i)
+	    {
+		matrix[i][j] = matrix[j][i]
+	    }
+	    else if (j > i)
+	    {
+		edges.add(Math.floor(Math.rand()*20) - 1)
+	    }
+	}
+	matrix.add(edges)
+    }
+
+    return {matrix: matrix}
+}
+
+function challenge_four_verify(value, answer)
+{
+    var answerLength = parseInt(answer)
+
+    var shortestLen = undefined
+    for (i = 0; i < value.matrix.length; ++i)
+    {
+	var len = shortestPathLength(i, value.matrix, [])
+	if (len == undefined)
+	    continue
+
+	if (len < shortestLen)
+	{
+	    shortestLen = len
+	}
+    }
+    
+    if (shortestLen == undefined && answerLength == -1)
+	return true
+    
+    return shortestLen == answerLength
+}
+
+//Find the shortest path starting at node
+function shortestPathLength(node, matrix, path)
+{
+    var line = matrix[node]
+
+    //This is the last node in the path
+    if (path.length == line.length)
+	return 0
+    
+    var shortestLen = undefined
+    for (i = 0; i < line.length;  ++i)
+    {
+	//Don't traverse to ourselves
+	if (i == node)
+	    continue
+	//If the node has been visited, don't traverse
+	if (path.contains(i))
+	    continue
+	//If there is no edge, don't traverse
+	if (line[i] == -1)
+	    continue
+	
+	var newLen = shortestPath(i, matrix, path + [i])
+	if (newLen == undefined)
+	    continue
+	
+	newLen = newLen + line[i]
+	if (shortest == undefined || newLen < shortestLen)
+	{
+	    shortestLen = newLen
+	}
+    }
+
+    if (shortestLen == undefined)
+	return undefined
+    
+    return shortestLen
 }
